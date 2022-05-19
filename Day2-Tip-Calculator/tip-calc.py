0d3942426db468a6f49cea23a0f93500156e80c9
Preethi Muthukumaran <preethi.m@berkeley.edu>
2022-05-19 13:58:21 -0700

    Cleaned up day 2 project. In line type conversions and use of f-strings.

--- Day2-Tip-Calculator/tip-calc.py
+++ Day2-Tip-Calculator/tip-calc.py
@@ -1,12 +1,6 @@
 print("Welcome to the tip calculator!\n")
-bill = input("What was the total bill? $")
-bill = float(bill)
-percentage = input("What percentage tip would you like to give? 10, 20, or 30?")
-percentage = int(percentage)
-percentage = percentage/100
-num_people = input("How many people will be splitting the bill?")
-num_people = int(num_people)
-cost_per_person = (bill*(1 + percentage))/num_people
-cost_per_person = round(cost_per_person, 2)
-cost_per_person = str(cost_per_person)
-print("Each person should pay: $" + cost_per_person)
+bill = float(input("What was the total bill? $"))
+percentage = int(input("What percentage tip would you like to give? 10, 20, or 30?"))/100
+num_people = int(input("How many people will be splitting the bill?"))
+cost_per_person = round( ((bill*(1 + percentage))/num_people), 2)
+print(f"Each person should pay: ${cost_per_person}")
