3fc381c07c4fd18b6398a33447736f9749eeed1f
Preethi Muthukumaran <preethi.m@berkeley.edu>
2022-05-19 18:53:03 -0700

    Completed Day3 Challenge: created a 'choose-you-own-adventure' game you can play in your terminal!

---
+++ Day3-Choose-Your-Own-Adventure/adventure.py
@@ -0,0 +1,131 @@
+import time
+
+print("""
+X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X
+|                           ,,'``````````````',,                            |
+X                        ,'`                   `',                          X
+|                      ,'                         ',                        |
+X                    ,'          ;       ;          ',                      X
+|       (           ;             ;     ;             ;     (               |
+X        )         ;              ;     ;              ;     )              X
+|       (         ;                ;   ;                ;   (               |
+X        )    ;   ;    ,,'```',,,   ; ;   ,,,'```',,    ;   ;               X
+|       (    ; ',;   '`          `',   ,'`          `'   ;,' ;              |
+X        )  ; ;`,`',  _--~~~~--__   ' '   __--~~~~--_  ,'`,'; ;     )       X
+|       (    ; `,' ; :  /       \~~-___-~~/       \  : ; ',' ;     (        |
+X  )     )   )',  ;   -_\  o    /  '   '  \    o  /_-   ;  ,'       )   (   X
+| (     (   (   `;      ~-____--~'       '~--____-~      ;'  )     (     )  |
+X  )     )   )   ;            ,`;,,,   ,,,;',            ;  (       )   (   X
+| (     (   (  .  ;        ,'`  (__ '_' __)  `',        ;  . )     (     )  |
+X  )     \/ ,".). ';    ,'`        ~~ ~~        `',    ;  .(.", \/  )   (   X
+| (   , ,'|// / (/ ,;  '        _--~~-~~--_        '  ;, \)    \|', ,    )  |
+X ,)  , \/ \|  \\,/  ;;       ,; |_| | |_| ;,       ;;  \,//  |/ \/ ,   ,   X
+|",   .| \_ |\/ |#\_/;       ;_| : `~'~' : |_;       ;\_/#| \/| _/ |.   ,"  |
+X#(,'  )  \\\#\ \##/)#;     :  `\/       \/   :     ;#(\##/ /#///  (  ',)# ,X
+|| ) | \ |/ /#/ |#( \; ;     :               ;     ; ;/ )#| \#\ \| / | ( |) |
+X\ |.\\ |\_/#| /#),,`   ;     ;./\_     _/\.;     ;   `,,(#\ |#\_/| //.| / ,X
+| \\_/# |#\##/,,'`       ;     ~~--|~|~|--~~     ;       `',,\##/#| #\_// \/|
+X  ##/#  #,,'`            ;        ~~~~~        ;            `',,#  #\##  //X
+|####@,,'`                 `',               ,'`                 `',,@####| |
+X#,,'`                        `',         ,'`                        `',,###X
+|'  spb                          ~~-----~~                               `' |
+X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X
+""")
+time.sleep(3)
+print("\n")
+print("Welcome to Dante's Inferno. Can you escape?\n")
+time.sleep(3)
+print("""You wake up, bound up and gagged on the deck of the Black Pearl!
+You crane your neck to see Captain Hector Barbossa steering the ship.
+You look around you and see a bunch of other people.
+Catching a glimpse beyond the boat, you see that you are on the river Styx headed towards the entrance to Hell! \n 
+Barbossa must have mistaken you for dead.\n""")
+time.sleep(10)
+print("Oh no. 💀💀\n")
+time.sleep(3)
+choice = input("""Knowing it is nearly impossibe to leave Hell once you pass Cerberus, you must make a choice. Do you...\n
+A) Jump up, attempt to overthrow Barbossa. \n
+B) Throw yourself overboard and attempt to swim back up the Styx. \n
+C) Play dead and hope you figure something out once you arrive at Hell. \n
+Pick A, B, or C: \n
+""").lower()
+if choice == "a":
+    print("Get over yourself, you're not Captain Jack Sparrow! Game over.")
+    exit(0)
+elif choice == "b":
+    print("Sorry, no mortal survives after touching the river Styx. Game over.")
+    exit(0)
+elif choice == "c":
+    print("You decide to take your chances in Hell. Hopefully you're quick on your feet!")
+else:
+    print("That is not a valid choice. Game over.")
+    exit(0)
+time.sleep(3)
+print("\n")
+print("""You land on the banks of the Styx. where many spirits are meandering about.
+Looking around Hell, you see R trying to sell her stupid cupcakes, thinking to yourself, 'Totally saw that one coming'.
+In the distance, an imposing castle made from obsidian towers over you. Suddenly, Barbossa is at your side and whisks you away to the castle.
+""")
+time.sleep(7)
+print("""
+                           ____                       ____
+                          |####|_   _   _   _   _   _|####|
+ _    __    __    _       |####| '-' '-' '-' '-' '-' |####|
+|#|__|##|__|##|__|#|      |====|=====================|====|       O
+|__|__|__[]__|__|__|      `.###|'._.'._.'._.'._.'._.'|###.'      /#\
+|_|__|__|__|__|__|_|        `.#|OoOOooOoOOoOoOOooOoOO|#.'       /###\
+ \================/ _   _   _ \=======================/ _   _  /_####\
+  \'._.'.__.'._.'/_| |_| |_| |_|     _               |_| |_| |_| |####\
+   |    .--.    |==============|   .'|'.       _     |===========/#####\
+   |    |  |    |OOoOOOooOooOoo|   |-+-|     .'|'.   |OOoOOOOoO||=====|
+   |    |__|    |oOoooOoOOOoOoO|   |_|_|     |-+-|   |oOooOooOo|| .-. |
+   |    ====    |==============|   =====     |_|_|   |=========|| | | |
+   |            |'._.'.__.'._.'|             =====   |'._.'._.'|| | | |
+  [==============]         .--[=======================]--.     || |_| |
+   |._.'.__.'._.|      _    `.[=======================].' .-.  || === |
+   |            |    .'|'.     ||.-.    _.o._    .-.||    | |  ||     |
+   |            |    |-+-|     ||| |  _)  =  (_  | |||    | |  ||======]
+   |    .--.    |    |_|_|     ||| |  )  ~@~  (  | |||    |_|  ||####.`
+   |    |  |    |    =====     |||_|   \  =  /   |_|||    ===  ||##.'
+   |    |__|    |              ||===    '._.'    ===||         ||.'
+   |    ====    |==============|| _________________ ||=========||
+   |            |'._.'.__.'._.'||[_________________]||'._.'._.'||
+  [==============]   _    _    || ]| /    |    \ |[ ||    _    ||
+   \============|  .'|'..'|'.  || ]|/     |     \|[ ||  .'|'.  ||
+    \___________|  |-+-||-+-|  || ]|      |      |[ ||  |-+-|  ||
+       ||          |_|_||_|_|  || ]|      |      |[ ||  |_|_|  ||
+       ||          ==========  || ]|     ~|~     |[ ||  =====  ||
+   ,,  ||                      || ]|      |      |[ ||         ||  ,,
+  ,;;,[======================,,|| ]|      |      |[ ||,,=========],;;,
+ ,;;;;,||OooOOOooOoOoooOooOO,;;,|_]|______|______|[_|,;;,OoOoOo||,;;;;,
+ .----.||OooOoOooOooOoOoOOo,;;;;,/_________________\,;;;;,OOolc||.----.
+ '.__.'====================.----.[_________________].----.======='.__.'
+ [_________________________'.__.'/                 \'.__.'____________]
+                           [____]                   [____]
+""")
+time.sleep(3)
+print("""Once in the castle, you see a throne. And sitting on his throne is Hades himself!
+At once he notices something is off. He beckons you forward.\n
+""")
+time.sleep(3)
+print("""Hades: 'You are still alive, aren't you?'""")
+time.sleep(1)
+print("*You nod nervously*")
+time.sleep(1)
+print("""Hades: 'Well, that was unexpected. Given that you like pineapple on pizza and you were going to end up here anyway, you have two options: \n """)
+time.sleep(1)
+choice = input("""
+A) Renounce pineapple on pizza, seek repentance, go back to your old life and maybe you'll end up in a more favorable situation.  
+B) You could never give up pineapple on pizza. You would rather stay.\n 
+What do you choose, A or B: \n
+""").lower()
+if choice == "a":
+    print("Good choice. You are sent back and you lead a happy and virtuous life in which you make normal pizza choices.")
+else:
+    print("Alright. Well it was good knowing you!")
+
+print("""\n
+Till next time!
+""")
+exit(0)
+
